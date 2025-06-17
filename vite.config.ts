b7785fc36f7e0705aaa2561b8341b070fc18e615
Joya1910 <techgaboo910@gmail.com>
2025-06-18 02:11:53 +0530

    Add CNAME and fix paths for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', 
+  base: '/', // instead of base: '/KE-Dental/',
   optimizeDeps: {
     exclude: ['lucide-react'],
     
